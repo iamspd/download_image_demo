db13d0d64e2969ea37af559450af72010061727e
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-07-30 14:07:45 -0400

    onClickMeClick added - MainActivity.java

--- app/src/main/java/com/example/downloadimagedemo/MainActivity.java
+++ app/src/main/java/com/example/downloadimagedemo/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.downloadimagedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // widgets
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMeClick(View view) {
+        Log.i("Message: ", "You clicked the button");
     }
 }
